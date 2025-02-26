f721e5197caf72787c0eb2d857d59c36c7b79be1
Jatin <jatinsojitra21@gmail.com>
2025-02-26 23:12:10 +0530

    1331. Rank Transform of an Array leet code Problems

--- sp_52.js
+++ sp_52.js
@@ -1,11 +1,11 @@
+// 1331. Rank Transform of an Array
+
 var arrayRankTransform = function(arr) {
     let sortedArr = [...new Set(arr)].sort((a, b) => a - b); // Sort unique numbers
     let rankMap = new Map();
     
-    // Assign ranks starting from 1
     sortedArr.forEach((num, index) => rankMap.set(num, index + 1));
 
-    // Replace each number in arr with its rank
     return arr.map(num => rankMap.get(num));
 };
 
